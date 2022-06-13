7b343e59e2d1d4761102aeb1768523492c4dc3a3
Matthew <matthewmedina0706@gmail.com>
2022-06-13 18:40:47 -0400

    Added image and bio to About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
-  
+import React, { Component, Profiler } from 'react';
+import "./About.css";
+import profilepic from "../assets/profilepic.jpg";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <img src={profilepic} alt="Profile Pic"/>
+        <h3>Matthew Medina</h3>
+        <p>I'm a rising senior at Marist College majoring in Computer Science</p>
       </div>
     )
   }
